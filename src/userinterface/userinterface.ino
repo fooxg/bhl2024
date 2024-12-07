298814c67a352ea2b694ec5d5be51b1cbf907724
DieRust <lesiakrafi@gmail.com>
2024-12-08 00:58:00 +0100

    made userinterface

--- src/userinterface/userinterface.ino
+++ src/userinterface/userinterface.ino
@@ -2,9 +2,11 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Keypad.h>
+#include <math.h>
 //init defines
 #define WIDTH 128
 #define HEIGHT 64
+#define MIDLINE 8+(HEIGHT-8)/2
 #define OLED_ADDR   0x3C
 
 //init struts
@@ -24,7 +26,6 @@ typedef struct userData{
 } UserData;
 
 //init own functions
-void data_abot_guests();
 void draw_main_interface(Time,UserData);
 
 //init keypad
@@ -37,7 +38,7 @@ char hexaKeys[ROWS][COLS] = {
   {'7','5','6','C'},
   {'*','0','#','D'}
 };
-byte rowPins[ROWS] = {2,0,4,16};; //connect to the row pinouts of the keypad
+byte rowPins[ROWS] = {2,0,4,16}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {17,5,18,19}; //connect to the column pinouts of the keypad
 
 //initialize an instance of class NewKeypad
@@ -49,52 +50,85 @@ Adafruit_SSD1306 display(WIDTH, HEIGHT, &Wire, -1);
 Time stime = {21,37};
 UserData GuideData = {"Guide",64,1030,120,2,90,34};
 UserData GuestData1 = {"Adam",87,1040,117,3,92,30};
-
+UserData GuestData2 = {"Maciek",100,1027,90,4,87,26};
+UserData GuestData3 = {"Rafal",34,1065,99,1,94,15};
+UserData ActualData = GuideData;
 uint8_t sec = 0;
 uint8_t battery = 64;
+
+
+
 void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
   Serial.begin(9600);
-  draw_main_interface(stime,GuideData);
+  customKeypad.addEventListener(keypadEvent);
 }
 
 void loop() {
-  char customKey = customKeypad.getKey();
-  
-  if (customKey){
-    switch(customKey){
-      case '1':
-          draw_main_interface(stime,GuideData);
-          break;
-      case '2':
-          draw_main_interface(stime,GuestData1);
-          break;
-    }
-  }
+  char key = customKeypad.getKey();
+  draw_main_interface(stime,ActualData);
 }
 
-void data_abot_guests(){
-  char customKey = customKeypad.getKey();
-  
-
-}
 void draw_main_interface(Time time,UserData userData){
   display.clearDisplay();
   display.setTextColor(WHITE);
   display.setTextSize(1);
+  //write upper line
   display.setCursor(0, 0);
   display.print(time.hour);
   display.print(":");
   display.print(time.min);
-  display.print("  ");
+  display.setCursor(WIDTH/2-(strlen(userData.name)*3),0);
   display.print(userData.name);
-  display.print("  ");
-  display.print(strlen(userData.name));
-  display.setCursor(WIDTH-15-3, 0);
-  display.print(userData.battery);
-  display.print("%");
-  //display.drawRect(int16_t x, int16_t y, int16_t w, int16_t h, white)
+  if(userData.battery < 100){
+    display.setCursor(WIDTH-15-3, 0);
+    display.print(userData.battery);
+    display.print("%");
+  }else{
+    display.setCursor(WIDTH-20-4, 0);
+    display.print(userData.battery);
+    display.print("%");
+  }
+  //
+  display.setCursor(0, 9);
+  display.print("Saturation");
+  display.setCursor(WIDTH/2+2, 9);
+  display.print("Presure");
+  display.setCursor(0,MIDLINE+1);
+  display.print("Heart rate");
+  display.setCursor(WIDTH/2+2, MIDLINE+1);
+  display.print("CO2");
+
+  display.setCursor(WIDTH/2-((int)log10(userData.saturation)+1)*12, 20);
+  display.setTextSize(2);
+  display.print(userData.saturation);
+  display.setCursor(WIDTH-((int)log10(userData.presure)+1)*12, 20);
+  display.print(userData.presure);
+  display.setCursor(WIDTH/2-((int)log10(userData.heart_rate)+1)*12,MIDLINE+12);
+  display.print(userData.heart_rate);
+  display.setCursor(WIDTH-((int)log10(userData.C02)+1)*12, MIDLINE+12);
+  display.print(userData.C02);
+
   display.drawLine(0, 8, WIDTH, 8, WHITE);
-  display.drawLine(WIDTH/2, 8, WIDTH/2, HEIGHT, WHITE);
+  display.drawLine(WIDTH/2, 9, WIDTH/2, HEIGHT, WHITE);
+  display.drawLine(0,MIDLINE,WIDTH,MIDLINE,WHITE);
   display.display();
+}
+void keypadEvent(KeypadEvent key){
+  if(customKeypad.getState() == PRESSED){
+  switch (key){
+    case '1':
+      ActualData = GuideData;
+      break;
+    case '2':
+      ActualData = GuestData1;
+      break;
+    case '3':
+      ActualData = GuestData2;
+      break;
+    case '4':
+      ActualData = GuestData3;
+      break;  
+  }
+  }
 }
